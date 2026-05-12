a49330ccc0043e2a4cbaaa8b3d171489d03960bf
byKoro <yuriludwig.email@gmail.com>
2026-05-12 00:23:08 -0300

    merge com o felipe

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import administrador.database.auth_service as auth_service
 import administrador.menus.menu_login as menu
 import administrador.database.json_manager as json_manager
 import administrador.menus.menu_admin as menu_admin
-
+import Recepcionista.Funções_recepcionista as Recepcionista
 
 usuario = {}
 
@@ -18,9 +18,7 @@ while True:
       print("Usuário ou senha inválido")
       usuario = input("Usuário: ")
       senha = input("Senha: ")
-      autenticado = auth_service.auth_user(usuario,senha)
-
-    usuario = autenticado
+      usuario = auth_service.auth_user(usuario,senha)
 
   elif op == '0':
     break
@@ -28,3 +26,5 @@ while True:
   if usuario["nivel"] == "Administrador":
     menu_admin.menu_admin()
 
+  if usuario["nivel"] == "Recepcionista":
+    Recepcionista.
