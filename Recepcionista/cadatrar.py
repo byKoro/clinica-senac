3207144b9c155bdb55936de7d7a6a520be1aecb9
Felipe <superhtr19@gmail.com>
2026-05-18 21:42:22 -0400

    Termino em casa

---
+++ Recepcionista/cadatrar.py
@@ -0,0 +1,9 @@
+from administrador.service.id_generator import id_generator as id_generator
+def cadastrar_pacientes():
+    nome = input("Digite o nome: ")
+    idade = int(input("Digite a idade do paciente: "))
+    endereço = input("Digite seu número de endereço: ")
+    numero = int(input("Digite o numero da casa: "))
+    cpf = int(input("Digite seu cpf:"))
+    id = int(input({id.generate_id("Digite seu id")}))
+    
